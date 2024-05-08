b76874e704b867df923a9265bb3dbf6158163ca0
delilar <delilar45@gmail.com>
2024-05-08 22:10:00 +0300

    Сделал добавление данных через dialog и удаление элементов по кнопке

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const myLibrary = []
+let addButton = document.querySelector('.add');
+let deleteButtons = document.querySelectorAll('.delete');
+let popUp = document.getElementById('pop-up');
+let closePopUp = document.querySelector('.close-pop-up');
+let inputs = popUp.getElementsByTagName('input');
+
+const myLibrary = [];
 
 function Book() {
 
@@ -7,3 +13,11 @@ function Book() {
 function addBookToLibrary() {
     
 }
+
+addButton.addEventListener('click', () => {
+    popUp.showModal()
+})
+
+closePopUp.addEventListener('click', () => {
+    popUp.close()
+})
