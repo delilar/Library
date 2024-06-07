36345b7bfde0ddd74c6ff08af45e56924a3f409e
delilar <delilar45@gmail.com>
2024-06-07 11:02:38 +0300

    скрипт был переписан на классы

--- script.js
+++ script.js
@@ -7,68 +7,63 @@ const confirmPopUp = document.querySelector('.confirm-pop-up');
 const inputs = Array.from(popUp.getElementsByTagName('input'));
 let tableBody = document.getElementById('table-body');
 
+class Library {
+    constructor(author, name, publishing, year, pages) {
+        this.author = author;
+        this.name = name;
+        this.publishing = publishing;
+        this.year = year;
+        this.pages = pages;
+    }
 
-const myLibrary = [];
-
-function Book(author, name, publishing, year, pages) {
-    this.author = author;
-    this.name = name;
-    this.publishing = publishing;
-    this.year = year;
-    this.pages = pages;
-}
-
-function addBookToLibrary() {
-    const inputValues = inputs.map( elem => {
-        return elem.value === '' ? '\u2063' : elem.value; //Добавить обработку пустых строк
-    })
-    let book = new Book(...inputValues);
-
-    console.log(book)
-
-    const tableLine = `<tr class=table-child-${deleteButtons.length + 1}>
-                            <td>${book.author}</td>
-                            <td>${book.name}</td>
-                            <td>${book.publishing}</td>
-                            <td>${book.year}</td>
-                            <td>${book.pages}</td>
-                        </tr>`;
+    addBookToLibrary() {
+        const tableLine = `<tr class=table-child-${deleteButtons.length + 1}>
+                                <td>${this.author}</td>
+                                <td>${this.name}</td>
+                                <td>${this.publishing}</td>
+                                <td>${this.year}</td>
+                                <td>${this.pages}</td>
+                            </tr>`;
+        
+        tableBody.innerHTML += tableLine;
     
-    tableBody.innerHTML += tableLine;
-
-    buttons.innerHTML += `<button class="delete button-child-${deleteButtons.length + 1}" type="submit">Удалить</button>`;
+        buttons.innerHTML += `<button class="delete button-child-${deleteButtons.length + 1}" type="submit">Удалить</button>`;
+        
+        // Добавляем новые кнопки удаления в список deleteButtons
+        deleteButtons = document.querySelectorAll('.delete');
     
-    // Добавляем новые кнопки удаления в список deleteButtons
-    deleteButtons = document.querySelectorAll('.delete');
-
-    // Добавляем обработчики событий на новые кнопки удаления
-    deleteButtons.forEach(elem => {
-        elem.addEventListener('click', (e) => {
-            const elemToDelete = e.target.classList[e.target.classList.length - 1];
-            deleteBookFromLibrary(elemToDelete);
+        // Добавляем обработчики событий на новые кнопки удаления
+        deleteButtons.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                const elemToDelete = e.target.classList[e.target.classList.length - 1];
+                this.deleteBookFromLibrary(elemToDelete);
+            });
         });
-    });
+    }
+
+
+    deleteBookFromLibrary(elemToDelete) {
+        const buttonToDelete = document.querySelector(`.${elemToDelete}`);
+        const number = elemToDelete[elemToDelete.length - 1];
+        const tableRowToDelete = document.querySelector(`.table-child-${number}`);
+    
+        tableBody.removeChild(tableRowToDelete);
+        buttons.removeChild(buttonToDelete);
+    }
 }
 
-function deleteBookFromLibrary(elemToDelete) {
-    const buttonToDelete = document.querySelector(`.${elemToDelete}`);
-    const number = elemToDelete[elemToDelete.length - 1];
-    const tableRowToDelete = document.querySelector(`.table-child-${number}`);
+const inputValues = inputs.map( elem => {
+    return elem.value === '' ? '\u2063' : elem.value; //Добавить обработку пустых строк
+})
 
-    console.log(elemToDelete)
-    console.log(buttonToDelete);
-    console.log(tableRowToDelete);
-
-    tableBody.removeChild(tableRowToDelete);
-    buttons.removeChild(buttonToDelete);
-}
+const book = new Library(...inputValues)
 
 addButton.addEventListener('click', () => {
     popUp.showModal();
 })
 
 confirmPopUp.addEventListener('click', () => {
-    addBookToLibrary();
+    book.addBookToLibrary();
     popUp.close();
 })
 
@@ -78,7 +73,7 @@ closePopUp.addEventListener('click', () => {
 
 deleteButtons.forEach(elem => {
     elem.addEventListener('click', (e) => {
-        const elemToDelete = e.target.classList[e.target.classList.length-1]
-        deleteBookFromLibrary(elemToDelete);
+        const elemToDelete = e.target.classList[e.target.classList.length-1];
+        book.deleteBookFromLibrary(elemToDelete);
     })
 })
